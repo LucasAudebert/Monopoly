31cc992de23246e867660bfb2ecbd6f88b543bb9
blachert <blachert@pc-dg-039-12>
2016-05-24 12:37:37 +0200

    Apero #3

--- src/Data/EnumerationsMonopoly.java
+++ src/Data/EnumerationsMonopoly.java
@@ -13,9 +13,13 @@ public class EnumerationsMonopoly {
     
     public static final String[] COULEUR_PROPRIETE = {"bleuFonce", "orange", "mauve", "violet", "bleuCiel", "jaune", "vert", "rouge"};
     
+
     public enum typeResultat {
         neRienFaire, achat, loyer
     }
+
+    public static final String[] TYPE_RESULTAT = {"achat", "loyer", "neRienFaire"};
+
     
     public static final String[] TYPE_CARREAU = {"gare", "compagnie", "proprieteAConstruire", "autre"};
     
