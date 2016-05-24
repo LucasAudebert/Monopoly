a701ede6af6bb868cd0c60e5a00d9e632aab48b7
blachert <blachert@pc-dg-039-12>
2016-05-24 09:05:53 +0200

    Apero #2.5

--- src/Data/ProprieteAConstruire.java
+++ src/Data/ProprieteAConstruire.java
@@ -16,11 +16,5 @@ public class ProprieteAConstruire extends Propriete{
          }else{
            return  getLoyer();
          }
-         
-    }
-
-    @Override
-    public Resultat action(Joueur joueur) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
