9fb503008719597ea799b91da29da9787140368d
blachert <blachert@pc-dg-039-12>
2016-05-31 15:16:07 +0200

    Apero #3

--- src/Data/EnumerationsMonopoly.java
+++ src/Data/EnumerationsMonopoly.java
@@ -14,6 +14,10 @@ public class EnumerationsMonopoly {
     public enum COULEUR_PROPRIETE {
         bleuFonce, orange, mauve, violet, bleuCiel, jaune, vert, rouge
     }
+    
+    public static int[] COULEUR_CODE = {
+        1
+    };
 
     public enum TYPE_RESULTAT {
         neRienFaire, achat, loyer, autreCarreau
