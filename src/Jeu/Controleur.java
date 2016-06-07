e9a1d6040ce2f90f8a7b698d84cb41900a421054
blachert <blachert@pc-dg-039-12>
2016-06-07 14:54:14 +0200

    Achat de maisons

--- src/Jeu/Controleur.java
+++ src/Jeu/Controleur.java
@@ -198,12 +198,6 @@ public class Controleur {
                 monopoly.removePrisonnier(joueur);
                 joueur.reinitCompteurEssaiPrison();
             }
-            if(joueur.getDerniereValeurDes() == 20){
-                for(ProprieteAConstruire pac : joueur.getProprieteAConstruires()){
-                    monopoly.construireMaison(pac, joueur);
-                    
-                }
-            }
                 //Si joueur en prison et des non double et compteur < 3 => compteur++ et ne joue pas 
             if(monopoly.estEnPrison(joueur) && joueur.getCompteurEssaiPrison() < 3 ){
                 joueur.incrementCompteurEssaiPrison();
